400bba93b161f940807400d20c4c680ed6bfd327
ruboarm <ruboarm@gmail.com>
2022-10-18 18:52:49 +0400

    Adding first models by reading xml file

---
+++ Data/Models/ConfigData.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "configData")]
+    public class ConfigData
+    {
+        [XmlAttribute(AttributeName = "dnPrefix")]
+        public string DnPrefix { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+
+        [XmlElement(ElementName = "SubNetwork")]
+        public SubNetwork SubNetwork { get; set; }
+    }
+}
---
+++ Data/Models/VsDataContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "vsDataContainer", Namespace = "genericNrm.xsd")]
+    public class VsDataContainer
+    {
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+
+        [XmlElement(ElementName = "attributes")]
+        public Attributes Attributes { get; set; }
+
+        [XmlElement(ElementName = "vsDataContainer", Namespace = "genericNrm.xsd")]
+        public VsDataContainer VsDataContainerChild { get; set; } //
+    }
+}
