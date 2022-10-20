341febfc1d32ab11442495efa72ec782ff005990
ruboarm <ruboarm@gmail.com>
2022-10-20 14:35:41 +0400

    Adding ExternalUtranCell

---
+++ Data/Models/CellCapability.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "cellCapability", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+    public class CellCapability
+    {
+        [XmlElement(ElementName = "hsdschSupport", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int HsdschSupport { get; set; }
+
+        [XmlElement(ElementName = "edchSupport", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int EdchSupport { get; set; }
+
+        [XmlElement(ElementName = "edchTti2Support", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int EdchTti2Support { get; set; }
+
+        [XmlElement(ElementName = "enhancedL2Support", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int EnhancedL2Support { get; set; }
+
+        [XmlElement(ElementName = "fdpchSupport", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int FdpchSupport { get; set; }
+
+        [XmlElement(ElementName = "multiCarrierSupport", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int MultiCarrierSupport { get; set; }
+
+        [XmlElement(ElementName = "cpcSupport", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int CpcSupport { get; set; }
+
+        [XmlElement(ElementName = "qam64MimoSupport", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int Qam64MimoSupport { get; set; }
+    }
+}
---
+++ Data/Models/ExternalUtranCellAttributes.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "attributes", Namespace = "utranNrm.xsd")]
+    public class ExternalUtranCellAttributes
+    {
+        [XmlElement(ElementName = "rac", Namespace = "utranNrm.xsd")]
+        public int Rac { get; set; }
+
+        [XmlElement(ElementName = "lac", Namespace = "utranNrm.xsd")]
+        public int Lac { get; set; }
+
+        [XmlElement(ElementName = "primaryCpichPower", Namespace = "utranNrm.xsd")]
+        public int PrimaryCpichPower { get; set; }
+
+        [XmlElement(ElementName = "primaryScramblingCode", Namespace = "utranNrm.xsd")]
+        public int PrimaryScramblingCode { get; set; }
+
+        [XmlElement(ElementName = "uarfcnDl", Namespace = "utranNrm.xsd")]
+        public int UarfcnDl { get; set; }
+
+        [XmlElement(ElementName = "uarfcnUl", Namespace = "utranNrm.xsd")]
+        public int UarfcnUl { get; set; }
+
+        [XmlElement(ElementName = "mnc", Namespace = "utranNrm.xsd")]
+        public int Mnc { get; set; }
+
+        [XmlElement(ElementName = "mcc", Namespace = "utranNrm.xsd")]
+        public int Mcc { get; set; }
+
+        [XmlElement(ElementName = "rncId", Namespace = "utranNrm.xsd")]
+        public int RncId { get; set; }
+
+        [XmlElement(ElementName = "cId", Namespace = "utranNrm.xsd")]
+        public int CId { get; set; }
+
+        [XmlElement(ElementName = "userLabel", Namespace = "utranNrm.xsd")]
+        public string UserLabel { get; set; }
+    }
+}
