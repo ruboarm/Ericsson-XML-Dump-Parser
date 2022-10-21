19c8897c7fba2081d8cb652d5a6b75ace9dbd96f
ruboarm <ruboarm@gmail.com>
2022-10-21 10:22:48 +0400

    Add ExternalUtranFreq into Data.Models

---
+++ Data/Models/VsDataExternalUtranFreq1.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "vsDataExternalUtranFreq", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+    public class VsDataExternalUtranFreq
+    {
+        [XmlElement(ElementName = "userLabel", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public string UserLabel { get; set; }
+
+        [XmlElement(ElementName = "arfcnValueUtranDl", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ArfcnValueUtranDl { get; set; }
+    }
+}
