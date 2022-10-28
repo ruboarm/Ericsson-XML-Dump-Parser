0c3f26b09a95d619fba8055f23f2d9910c114d33
ruboarm <ruboarm@gmail.com>
2022-10-28 18:59:31 +0400

    Adding "vsDataAdmissionControl" into Data.Models

---
+++ Data/Models/vsDataAdmissionControl.cs
@@ -0,0 +1,164 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "vsDataAdmissionControl", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+    public class vsDataAdmissionControl
+    {
+        [XmlElement(ElementName = "resourceReservationForDifferentiation", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ResourceReservationForDifferentiation { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary9", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary9 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp20", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp20 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp21", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp21 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary7", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary7 { get; set; }
+
+        [XmlElement(ElementName = "dlMbmsGbrRatio", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int DlMbmsGbrRatio { get; set; }
+
+        [XmlElement(ElementName = "dlAdmOverloadThr", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int DlAdmOverloadThr { get; set; }
+
+        [XmlElement(ElementName = "paArpOverride", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int PaArpOverride { get; set; }
+
+        [XmlElement(ElementName = "ulAdmDifferentiationThr", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int UlAdmDifferentiationThr { get; set; }
+
+        [XmlElement(ElementName = "resourceReservationForPAState", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ResourceReservationForPAState { get; set; }
+
+        [XmlElement(ElementName = "ulTransNwBandwidth", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int UlTransNwBandwidth { get; set; }
+
+        [XmlElement(ElementName = "nrOfPaConnReservationsPerCell", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int NrOfPaConnReservationsPerCell { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp4", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp4 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp5", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp5 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp2", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp2 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp3", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp3 { get; set; }
+
+        [XmlElement(ElementName = "arpBasedPreEmptionState", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ArpBasedPreEmptionState { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp10", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp10 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp1", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp1 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp11", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp11 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp12", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp12 { get; set; }
+
+        [XmlElement(ElementName = "dlTransNwBandwidth", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int DlTransNwBandwidth { get; set; }
+
+        [XmlElement(ElementName = "ulAdmOverloadThr", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int UlAdmOverloadThr { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp8", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp8 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp9", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp9 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp6", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp6 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp7", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp7 { get; set; }
+
+        [XmlElement(ElementName = "admNrRbDifferentiationThr", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int AdmNrRbDifferentiationThr { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary2", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public object ZzzTemporary2 { get; set; } // Unknown Type
+
+        [XmlElement(ElementName = "zzzTemporary1", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public object ZzzTemporary1 { get; set; } // Unknown Type
+
+        [XmlElement(ElementName = "admNrRrcDifferentiationThr", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int AdmNrRrcDifferentiationThr { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary4", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary4 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary3", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary3 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary6", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary6 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary5", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary5 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp14", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp14 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp13", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp13 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp16", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp16 { get; set; }
+
+        [XmlElement(ElementName = "admResourceMinQciPrio", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int AdmResourceMinQciPrio { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp15", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp15 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp18", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp18 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp17", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp17 { get; set; }
+
+        [XmlElement(ElementName = "nrOfRbReservationsPerPaConn", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int NrOfRbReservationsPerPaConn { get; set; }
+
+        [XmlElement(ElementName = "dlAdmDifferentiationThr", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int DlAdmDifferentiationThr { get; set; }
+
+        [XmlElement(ElementName = "zzzTemp19", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemp19 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary22", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary22 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary25", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary25 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary26", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary26 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary23", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary23 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary24", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ZzzTemporary24 { get; set; }
+
+        [XmlElement(ElementName = "lbAtoThresholdLevel2", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int LbAtoThresholdLevel2 { get; set; }
+
+        [XmlElement(ElementName = "lbAtoThresholdLevel1", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int LbAtoThresholdLevel1 { get; set; }
+    }
+}
