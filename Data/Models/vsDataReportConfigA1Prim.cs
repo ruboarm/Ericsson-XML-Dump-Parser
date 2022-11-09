f3774be31abb609b525cc6fb4323c05f1b49ef69
ruboarm <ruboarm@gmail.com>
2022-11-10 00:09:07 +0400

    Adding "vsDataReportConfigA1Prim" into Data.Models

---
+++ Data/Models/vsDataReportConfigA1Prim.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "vsDataReportConfigA1Prim", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+    public class vsDataReportConfigA1Prim
+    {
+        [XmlElement(ElementName = "timeToTriggerA1Prim", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int timeToTriggerA1Prim { get; set; }
+
+        [XmlElement(ElementName = "hysteresisA1Prim", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int hysteresisA1Prim { get; set; }
+
+        [XmlElement(ElementName = "a1ThresholdRsrqPrim", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int a1ThresholdRsrqPrim { get; set; }
+
+        [XmlElement(ElementName = "a1ThresholdRsrpPrim", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int a1ThresholdRsrpPrim { get; set; }
+    }
+}
