905badedbe3ffa5946ae9f8712ac42ae966920f4
ruboarm <ruboarm@gmail.com>
2022-11-10 00:03:07 +0400

    Adding "vsDataUeMeasControl" into Data.Models

---
+++ Data/Models/vsDataUeMeasControl.cs
@@ -0,0 +1,185 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "vsDataUeMeasControl", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+    public class vsDataUeMeasControl
+    {
+        [XmlElement(ElementName = "maxMeasCdma20001xRtt", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int maxMeasCdma20001xRtt { get; set; }
+
+        [XmlElement(ElementName = "allowReleaseQci1", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool allowReleaseQci1 { get; set; }
+
+        [XmlElement(ElementName = "measQuantityUtraFDD", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int measQuantityUtraFDD { get; set; }
+
+        [XmlElement(ElementName = "csfbHoTargetSearchTimer", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int csfbHoTargetSearchTimer { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary7", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary7 { get; set; }
+
+        [XmlElement(ElementName = "ueMeasurementsActiveGERAN", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool ueMeasurementsActiveGERAN { get; set; }
+
+        [XmlElement(ElementName = "a5B2MobilityTimer", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int a5B2MobilityTimer { get; set; }
+
+        [XmlElement(ElementName = "ueMeasurementsActiveCDMA2000", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool ueMeasurementsActiveCDMA2000 { get; set; }
+
+        [XmlElement(ElementName = "maxMeasCdma2000eHrpd", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int maxMeasCdma2000eHrpd { get; set; }
+
+        [XmlElement(ElementName = "ueMeasurementsActive", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool ueMeasurementsActive { get; set; }
+
+        [XmlElement(ElementName = "maxMeasInterFreqEUtra", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int maxMeasInterFreqEUtra { get; set; }
+
+        [XmlElement(ElementName = "sMeasure", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int sMeasure { get; set; }
+
+        [XmlElement(ElementName = "maxMeasUtraTdd", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int maxMeasUtraTdd { get; set; }
+
+        [XmlElement(ElementName = "filterCoefficientEUtraRsrq", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int filterCoefficientEUtraRsrq { get; set; }
+
+        [XmlElement(ElementName = "ueMeasurementsActiveIF", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool ueMeasurementsActiveIF { get; set; }
+
+        [XmlElement(ElementName = "maxMeasEUtraOther", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int maxMeasEUtraOther { get; set; }
+
+        [XmlElement(ElementName = "filterCoefficientEUtraRsrp", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int filterCoefficientEUtraRsrp { get; set; }
+
+        [XmlElement(ElementName = "maxMeasGeran", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int maxMeasGeran { get; set; }
+
+        [XmlElement(ElementName = "servOrPrioIFHoTimer", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int servOrPrioIFHoTimer { get; set; }
+
+        [XmlElement(ElementName = "maxMeasUtraFdd", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int maxMeasUtraFdd { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary2", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int? zzzTemporary2 { get; set; } // Unknown Type
+
+        [XmlElement(ElementName = "maxUtranCellsToMeasure", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int maxUtranCellsToMeasure { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary1", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int? zzzTemporary1 { get; set; } // Unknown Type
+
+        [XmlElement(ElementName = "zzzTemporary4", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int? zzzTemporary4 { get; set; } // Unknown Type
+
+        [XmlElement(ElementName = "zzzTemporary3", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int? zzzTemporary3 { get; set; } // Unknown Type
+
+        [XmlElement(ElementName = "searchEffortTime", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int? searchEffortTime { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary6", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int? zzzTemporary6 { get; set; }
+
+        [XmlElement(ElementName = "ueMeasurementsActiveCDMA20001xRtt", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool ueMeasurementsActiveCDMA20001xRtt { get; set; }
+
+        [XmlElement(ElementName = "ueMeasurementsActiveUTRAN", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool ueMeasurementsActiveUTRAN { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary5", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary5 { get; set; }
+
+        [XmlElement(ElementName = "inhibitB2RsrqConfig", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool inhibitB2RsrqConfig { get; set; }
+
+        [XmlElement(ElementName = "bothA5RsrpRsrqCheck", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool bothA5RsrpRsrqCheck { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary9", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary9 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary8", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary8 { get; set; }
+
+        [XmlElement(ElementName = "lowPrioMeasThresh", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int lowPrioMeasThresh { get; set; }
+
+        [XmlElement(ElementName = "excludeInterFreqAtCritical", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool excludeInterFreqAtCritical { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary10", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary10 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary11", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary11 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary12", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary12 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary13", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary13 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary14", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary14 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary15", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary15 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary16", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary16 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary17", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary17 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary18", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary18 { get; set; }
+
+        [XmlElement(ElementName = "filterCoefficientEUtraUlSinrMax", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int filterCoefficientEUtraUlSinrMax { get; set; }
+
+        [XmlElement(ElementName = "maxNoMeasReportsInact", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int maxNoMeasReportsInact { get; set; }
+
+        [XmlElement(ElementName = "filterCoefficientEUtraNI", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int filterCoefficientEUtraNI { get; set; }
+
+        [XmlElement(ElementName = "ulSinrOffset", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int ulSinrOffset { get; set; }
+
+        [XmlElement(ElementName = "targetA2UlSearchOffset", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int targetA2UlSearchOffset { get; set; }
+
+        [XmlElement(ElementName = "a3SuspendCsgTimer", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int a3SuspendCsgTimer { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary21", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary21 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary22", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary22 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary25", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary25 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary23", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary23 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary24", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary24 { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary26", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary26 { get; set; }
+
+        [XmlElement(ElementName = "checkA2SearchLevel", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool checkA2SearchLevel { get; set; }
+
+        [XmlElement(ElementName = "zzzTemporary27", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int zzzTemporary27 { get; set; }
+    }
+}
