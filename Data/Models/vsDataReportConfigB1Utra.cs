c369e15dca2b612dfa3f7c17c435c24ef3f54c0c
ruboarm <ruboarm@gmail.com>
2022-11-13 22:20:55 +0400

    Adding "vsDataReportConfigB1Utra" into Data.Models

---
+++ Data/Models/vsDataReportConfigB1Utra.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "vsDataReportConfigB1Utra", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+    public class vsDataReportConfigB1Utra
+    {
+        [XmlElement(ElementName = "b1ThresholdRscpUtra", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int b1ThresholdRscpUtra { get; set; }
+
+        [XmlElement(ElementName = "timeToTriggerB1", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int timeToTriggerB1 { get; set; }
+
+        [XmlElement(ElementName = "hysteresisB1", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int hysteresisB1 { get; set; }
+
+        [XmlElement(ElementName = "b1ThresholdEcNoUtra", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int b1ThresholdEcNoUtra { get; set; }
+    }
+}
