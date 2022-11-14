6ddbc9aeff00d701b2745fe799a9ec61d38893db
ruboarm <ruboarm@gmail.com>
2022-11-14 17:58:50 +0400

    Adding "vsDataReportConfigCsfbGeran" into Data.Models

---
+++ Data/Models/vsDataReportConfigCsfbGeran.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "vsDataReportConfigCsfbGeran", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+    public class vsDataReportConfigCsfbGeran
+    {
+        [XmlElement(ElementName = "thresholdGeran", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int thresholdGeran { get; set; }
+
+        [XmlElement(ElementName = "hysteresis", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int hysteresis { get; set; }
+
+        [XmlElement(ElementName = "timeToTrigger", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int timeToTrigger { get; set; }
+    }
+}
