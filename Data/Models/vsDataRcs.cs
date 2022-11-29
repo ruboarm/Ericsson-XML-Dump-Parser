174808df73d79af7c623dc8686a04143c81daa6f
ruboarm <ruboarm@gmail.com>
2022-11-29 23:09:13 +0400

    Adding "vsDataRcs" into Data.Models

---
+++ Data/Models/vsDataRcs.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "vsDataRcs", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+    public class vsDataRcs
+    {
+        [XmlElement(ElementName = "tInactivityTimer", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int tInactivityTimer { get; set; }
+
+        [XmlElement(ElementName = "rlcDlDeliveryFailureAction", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int rlcDlDeliveryFailureAction { get; set; }
+
+        [XmlElement(ElementName = "tInactivityTimerBr", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public int tInactivityTimerBr { get; set; }
+    }
+}
