055b57f14583cdee8f65fde7c85b957dfd556d8b
ruboarm <ruboarm@gmail.com>
2022-11-30 23:01:35 +0400

    Adding "vsDataSecurityHandling" into Data.Models

---
+++ Data/Models/vsDataSecurityHandling.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace Data.Models
+{
+    [XmlRoot(ElementName = "vsDataSecurityHandling", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+    public class vsDataSecurityHandling
+    {
+        [XmlElement(ElementName = "rbIdSupervisionActive", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool rbIdSupervisionActive { get; set; }
+
+        [XmlElement(ElementName = "integrityProtectAlgoPrio", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public List<int> integrityProtectAlgoPrios { get; set; }
+
+        [XmlElement(ElementName = "cipheringAlgoPrio", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public List<int> cipheringAlgoPrios { get; set; }
+
+        [XmlElement(ElementName = "countWrapSupervisionActive", Namespace = "EricssonSpecificAttributes.17.28.xsd")]
+        public bool countWrapSupervisionActive { get; set; }
+    }
+}
